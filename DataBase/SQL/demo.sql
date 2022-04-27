6d034c0c1aaf316260014445e1d522797fb998d8
weicheng <weicheng.Zhang@hofan.cn>
2022-04-27 15:34:31 +0800

    perf:sql

--- DataBase/SQL/demo.sql
+++ DataBase/SQL/demo.sql
@@ -16,3 +16,15 @@ CASE
     WHEN col_1 IN ('a', 'b') THEN 'The first'
     WHEN col_2 IN ('a') THEN 'The second'   -- 该条件被忽略 不会执行
 ELSE 'others' END
+
+-- 将县编号转换成地区编号
+SELECT CASE pred_name
+    WHEN '德岛' THEN `四国`
+    WHEN '香川' THEN `四国`
+    WHEN '爱媛' THEN `四国`
+    WHEN '高知' THEN `四国`
+    WHEN '福冈' THEN `九州`
+    WHEN '佐贺' THEN `九州`
+    WHEN '长崎' THEN `九州`
+ELSE '其他' END AS district, SUM(population)
+FORM `PobTbl`
